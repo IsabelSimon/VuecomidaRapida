8902f51719f39524ff637e3d082ec16dec75dc27
IsabelSimon <simon.isabel17@gmail.com>
2022-09-22 18:25:31 -0500

    Dark

--- src/components/Image.vue
+++ src/components/Image.vue
@@ -6,7 +6,7 @@
 
 <style scoped>
  div{
-        border: 8px solid #fff !important;
+        border: 8px solid #000 !important;
         border-radius: 10px !important;
         height:12em;
         color:transparent;
